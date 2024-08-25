8c539a515d82f690a01a49718c33ffaa91f6dc5b
ObiomaIkpe <obiomaikpe1.com>
2024-08-25 18:06:43 +0100

    portfolio site rebuild

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -16,6 +16,9 @@ export default {
         'gradient' : {
           to: { 'background-position': '200% center' },
         }
+      },
+      boxShadow: {
+        'red-shadow': '0px 15px 5px 3px rgba(10, 0, 0, 0.7)',
       }
     },
   },
