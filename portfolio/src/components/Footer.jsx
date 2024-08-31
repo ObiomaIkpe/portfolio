4af7d6d2fb7db785b93209e51363404c602dce0e
ObiomaIkpe <obiomaikpe1.com>
2024-08-31 10:50:29 +0100

    changes to the footer component

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -4,7 +4,7 @@
 const ComFooter = () => {
 
     return (
-        <div className="flex items-center gap-6 sm:mt-0 md:mt-4 sm:justify-center border-b-500 ">
+        <div className="flex items-center justify-center">
            <span className="font-800 text-xl">&copy; {new Date().getFullYear()}</span>           
         </div>
     )
